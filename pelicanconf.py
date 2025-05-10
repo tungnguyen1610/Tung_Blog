95f06ef167fdb21f3c170e0c61fd383af5cf5b95
tung <nguyenhoangtung1610@gmail.com>
2025-05-10 13:04:29 +0200

    Update config

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Tung Nguyen'
 SITENAME = 'T_Blog'
-SITEURL = 'https://tungnguyen1610.github.io/Tung_Blog/'
+SITEURL = ''
 
 PATH = 'content'
 TIMEZONE = 'Asia/Ho_Chi_Minh'
@@ -10,7 +10,9 @@ ARTICLE_PATHS = ['embedded_systems', 'digital_design', 'automotive']
 ARTICLE_URL = '{category}/{slug}.html'
 ARTICLE_SAVE_AS = '{category}/{slug}.html'
 
-THEME = 'notmyidea'
+AUTHOR_SAVE_AS = 'author/{slug}.html'
+AUTHOR_URL = 'author/{slug}.html'
+#THEME = 'notmyidea'
 THEME_TEMPLATES_OVERRIDES = ['templates']
 DEFAULT_PAGINATION = 10
 DISPLAY_CATEGORIES_ON_MENU = True

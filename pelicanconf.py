c3d4cdf3a80f35526dbc1d9f72e4b21bb66560a1
tung <nguyenhoangtung1610@gmail.com>
2025-05-11 16:07:18 +0200

    Update pelican config for github action

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Tung Nguyen'
 SITENAME = 'T_Blog'
-SITEURL = 'https://tungnguyen1610.github.io/Tung_Blog/'
+#SITEURL = 'https://tungnguyen1610.github.io/Tung_Blog/'
 #SITEURL=''
 PATH = 'content'
 TIMEZONE = 'Asia/Ho_Chi_Minh'
@@ -9,7 +9,17 @@ DEFAULT_LANG = 'en'
 ARTICLE_PATHS = ['embedded_systems', 'digital_design', 'automotive','programming']
 ARTICLE_URL = '{category}/{slug}.html'
 ARTICLE_SAVE_AS = '{category}/{slug}.html'
-
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+CACHE_CONTENT = True
+LOAD_CONTENT_CACHE = True
+CHECK_MODIFIED_METHOD = 'md5'
+GZIP_CACHE = False
+CONTENT_CACHING_LAYER = 'generator'
+WITH_FUTURE_DATES = False
 THEME = 'notmyidea'
 THEME_TEMPLATES_OVERRIDES = ['templates']
 DEFAULT_PAGINATION = 10
@@ -21,3 +31,4 @@ SOCIAL = (
     ('<i class="fab fa-linkedin"></i> LinkedIn', 'https://www.linkedin.com/in/tungbud/'),
     ('<i class="fas fa-envelope"></i> Email', 'mailto:nguyenhoangtung1610@gmail.com'),
 )
+

32ee42d9c3342de664e8df6a97f557d8d8ef7aa4
tung <nguyenhoangtung1610@gmail.com>
2025-05-11 15:56:37 +0200

    Add social media link

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,12 @@
 AUTHOR = 'Tung Nguyen'
 SITENAME = 'T_Blog'
 SITEURL = 'https://tungnguyen1610.github.io/Tung_Blog/'
-
+#SITEURL=''
 PATH = 'content'
 TIMEZONE = 'Asia/Ho_Chi_Minh'
 DEFAULT_LANG = 'en'
 
-ARTICLE_PATHS = ['embedded_systems', 'digital_design', 'automotive']
+ARTICLE_PATHS = ['embedded_systems', 'digital_design', 'automotive','programming']
 ARTICLE_URL = '{category}/{slug}.html'
 ARTICLE_SAVE_AS = '{category}/{slug}.html'
 
@@ -17,7 +17,7 @@ DISPLAY_CATEGORIES_ON_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 
 SOCIAL = (
-    ('GitHub', 'https://github.com/yourusername'),
-    ('LinkedIn', 'https://www.linkedin.com/in/yourusername'),
-    ('Facebook', 'https://www.facebook.com/yourusername'),
+    ('<i class="fab fa-github"></i> GitHub', 'https://github.com/tungnguyen1610'),
+    ('<i class="fab fa-linkedin"></i> LinkedIn', 'https://www.linkedin.com/in/tungbud/'),
+    ('<i class="fas fa-envelope"></i> Email', 'mailto:nguyenhoangtung1610@gmail.com'),
 )

ccff9d0883487ed37cc7863d19e90ab28e07b42b
tung <nguyenhoangtung1610@gmail.com>
2025-08-09 16:22:22 +0200

    Modify publishconf

--- publishconf.py
+++ publishconf.py
@@ -2,7 +2,9 @@ import os
 import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
-#RELATIVE_URLS = False
+
+SITEURL = 'https://tungnguyen1610.github.io/Tung_Blog'
+RELATIVE_URLS = False  # important for production so paths are absolute
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 DELETE_OUTPUT_DIRECTORY = True
